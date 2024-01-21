dff7e7dd17793f4e16538101024f7618ef69cb15
OmarTheDarkKnight <jayed258@gmail.com>
2024-01-20 23:04:59 -0600

    Add basic code

--- src/main/java/com/gotham/alfred/AlfredApplication.java
+++ src/main/java/com/gotham/alfred/AlfredApplication.java
@@ -1,4 +1,4 @@
-package com.example.alfred;
+package com.gotham.alfred;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/gotham/alfred/AlfredApplicationTests.java
+++ src/test/java/com/gotham/alfred/AlfredApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.alfred;
+package com.gotham.alfred;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
