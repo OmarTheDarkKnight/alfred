0c3371bb262d0b945452e34661d8482b67bb0045
OmarTheDarkKnight <jayed258@gmail.com>
2024-01-24 15:50:00 -0600

    Add basic data insertion test

--- src/test/java/com/waynetech/alfred/AlfredApplicationTests.java
+++ src/test/java/com/waynetech/alfred/AlfredApplicationTests.java
@@ -1,13 +1,51 @@
 package com.waynetech.alfred;
 
+import com.waynetech.alfred.order.Order;
+import com.waynetech.alfred.order.OrderRepository;
+import com.waynetech.alfred.user.User;
+import com.waynetech.alfred.user.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class AlfredApplicationTests {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
     @Test
-    void contextLoads() {
+    void saveUser() {
+
+        User testUser = new User();
+        testUser.setId(1);
+        testUser.setFirstName("Alfred");
+        testUser.setLastName("Pennyworth");
+        testUser.setEmail("alfred_pennyworth@wwaynetech.com");
+
+        User user = userRepository.save(testUser);
+        Optional<User> userInDB = userRepository.findById(user.getId());
+        assertTrue(userInDB.isPresent());
+    }
+
+    @Test
+    void saveOrder() {
+        Order testOrder = new Order();
+        testOrder.setId(1);
+        testOrder.setProductName("Earl Grey Black Tea");
+        testOrder.setOrderAmount(365);
+        testOrder.setUserId(1);
+
+        Order order = orderRepository.save(testOrder);
+        Optional<Order> orderInDB = orderRepository.findById(order.getId());
+        assertTrue(orderInDB.isPresent());
     }
 
 }
